9264c010bc6b993e4b99f45454ce4faeff1f3db9
Dagitt55 <50029583+Dagitt55@users.noreply.github.com>
2024-11-09 01:46:27 +0300

    Update views.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -5,7 +5,6 @@ from django.views.generic.detail import DetailView
 
 def list_books(request):
 	books = Book.objects.all()
-	context = {'list_books': books}
 	return render(request, "relationship_app/list_books.html", {'books': books})
 
 class LibraryDetailView(DetailView):
