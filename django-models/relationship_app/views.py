c8b597900e4a016d0174b9cb70fef0d7ba4c54dc
Dagitt55 <50029583+Dagitt55@users.noreply.github.com>
2024-11-09 01:16:05 +0300

    Update views.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Book
+
+def list_books(request):
+	books = Book.objects.all()
+	context = {'list_books': books}
+	return render(request, "relationship_app/list_books.html", "Book.objects.all()")
