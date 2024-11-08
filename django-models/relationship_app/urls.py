249e6a9d170d3beaf6d01d2705d2ca09814caf74
Dagitt55 <50029583+Dagitt55@users.noreply.github.com>
2024-11-09 01:55:17 +0300

    Update urls.py

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,10 +1,6 @@
 from django.urls import path
 from .views import list_books
 from . import views
-from django.contrib.auth.views import LoginView
-from django.urls import path
-from django.contrib.auth import login
-from django.contrib.auth.forms import UserCreationForm
 
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),
