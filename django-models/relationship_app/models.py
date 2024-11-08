7b53c23944a492bec40f2df17ca203b20081b949
Dagitt55 <50029583+Dagitt55@users.noreply.github.com>
2024-11-09 02:05:46 +0300

    Update models.py

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Author(models.Model):
 	name = models.CharField(max_length=100)
@@ -22,3 +23,13 @@ class Librarian(models.Model):
 	library = models.OneToOneField(Library, on_delete=models.CASCADE)
 	def __str__(self):
         return self.name
+ROLE_CHOICES = (
+    ('Admin', 'Admin'),
+    ('Librarian', 'Librarian'),
+    ('Member', 'Member'),
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)  # Link to Django's built-in User model
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+
+    def __str__(self):
+        return f'{self.user.username} ({self.role})'
