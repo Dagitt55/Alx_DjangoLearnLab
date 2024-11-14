7717b2bcb3043a01ca997e993f12d069354494b5
Dagitt55 <50029583+Dagitt55@users.noreply.github.com>
2024-11-15 00:16:11 +0300

    Add files via upload

---
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    publication_year = models.IntegerField("year published")
+
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField()
+    profile_photo = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100, **options)
+    
