3c6589b6c8d94e14f7c79918aa1632718d07b209
Dagitt55 <50029583+Dagitt55@users.noreply.github.com>
2024-11-21 15:48:50 +0300

    Update settings.py

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'api',
     'rest_framework',
+    'rest_framework.authtoken',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -40,6 +41,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',  # Use TokenAuthentication
+    ]
+    }
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
