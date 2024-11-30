041f3b2d500c5af44e3b045087371e6a9ac507b8
Dagitt55 <50029583+Dagitt55@users.noreply.github.com>
2024-11-30 05:07:44 +0300

    Update urls.py

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('books/', BookListView.as_view(), name='book-list'),  # List view
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),  # Detail view
     path('books/create/', BookCreateView.as_view(), name='book-create'),  # Create view
-    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),  # Update view
-    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),  # Delete view
+    path('books/update/', BookUpdateView.as_view(), name='book-update'),  # Update view
+    path('books/delete/', BookDeleteView.as_view(), name='book-delete'),  # Delete view
 ]
