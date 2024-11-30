01f1c1cd51d76840bdef6c30b2c9a82b03b95eaa
Dagitt55 <50029583+Dagitt55@users.noreply.github.com>
2024-11-30 05:10:54 +0300

    Update views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,6 +2,9 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import FilterSet, CharFilter, NumberFilter
+
 
 # List all books
 class BookListView(generics.ListAPIView):
@@ -32,3 +35,21 @@ class BookDeleteView(generics.DestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticated]  # Only authenticated users can delete books
+
+# Custom filter class for the Book model
+class BookFilter(FilterSet):
+    title = CharFilter(lookup_expr='icontains', label='Title')
+    author = CharFilter(lookup_expr='icontains', label='Author')
+    publication_year = NumberFilter(lookup_expr='exact', label='Publication Year')
+
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'publication_year']
+
+# Book List View with filtering
+class BookListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = BookFilter
