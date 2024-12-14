bcb8bfb4b9d20c462dba7b468a122616af83f0e3
Dagitt55 <50029583+Dagitt55@users.noreply.github.com>
2024-12-15 02:31:07 +0300

    Update serializers.py

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,26 +1,35 @@
 from rest_framework import serializers
-from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
+from django.contrib.auth import get_user_model
 
-User = get_user_model()  # Get the custom user model
+# Import your custom user model
+#CustomUser = get_user_model()
 
-
-class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)  # Ensure the password is write-only
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
 
     class Meta:
-        model = User
-        fields = ['id', 'username', 'email', 'password', 'bio', 'profile_picture']
-        extra_kwargs = {'password': {'write_only': True}}
+        model = get_user_model()
+        fields = ['username', 'email', 'password', 'bio', 'profile_picture']
 
     def create(self, validated_data):
-        # Create a user using the custom user model's manager
-        user = User.objects.create_user(
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
-            email=validated_data.get('email', ''),
+            email=validated_data['email'],
             password=validated_data['password'],
             bio=validated_data.get('bio', ''),
-            profile_picture=validated_data.get('profile_picture')
+            profile_picture=validated_data.get('profile_picture', None)
         )
-        Token.objects.create(user=user)  # Automatically create a token for the new user
+        
+        Token.objects.create(user=user)
+
+        
         return user
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+class TokenSerializer(serializers.Serializer):
+    token = serializers.CharField()
